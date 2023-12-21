f77d92f70441c756c8af9fa7295b8c9f010de519
minhng-178 <anhminh1782002@gmail.com>
2023-12-21 15:08:48 +0700

    query img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 import { ArrowDownToLine, CheckCircle, Leaf } from 'lucide-react';
 
+import ProductReel from '@/components/ProductReel';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+
 import { Button, buttonVariants } from '@/components/ui/button';
 
 const perks = [
@@ -9,18 +11,19 @@ const perks = [
     name: 'Instant Delivery',
     Icon: ArrowDownToLine,
     description:
-      'Pariatur labore in nulla mollit eiusmod amet eiusmod tempor consectetur sunt deserunt est laborum cillum.',
+      'Get your assets delivered to your email in seconds and download them right away.',
   },
   {
     name: 'Guaranteed Quality',
     Icon: CheckCircle,
     description:
-      'Culpa ex cillum id ullamco laborum minim adipisicing ad irure.',
+      'Every asset on our platform is verified by our team to ensure our highest quality standards. Not happy? We offer a 30-day refund guarantee.',
   },
   {
     name: 'For the Planet',
     Icon: Leaf,
-    description: 'Ipsum cupidatat eu labore excepteur nisi reprehenderit.',
+    description:
+      "We've pledged 1% of sales to the preservation and restoration of the natural environment.",
   },
 ];
 
@@ -34,22 +37,27 @@ export default function Home() {
             <span className="text-blue-600">digital assets</span>.
           </h1>
           <p className="mt-6 text-lg max-w-prose text-muted-foreground">
-            Welcome to DigitalHippo. Every asset on your platform is verified by
+            Welcome to DigitalHippo. Every asset on our platform is verified by
             our team to ensure our highest quality standards.
           </p>
           <div className="flex flex-col sm:flex-row gap-4 mt-6">
             <Link href="/products" className={buttonVariants()}>
               Browse Trending
             </Link>
-            <Button variant={'ghost'}>Our quality promise &rarr;</Button>
+            <Button variant="ghost">Our quality promise &rarr;</Button>
           </div>
         </div>
-        {/*TODO: List products*/}
+
+        <ProductReel
+          query={{ sort: 'desc', limit: 4 }}
+          href="/products?sort=recent"
+          title="Brand new"
+        />
       </MaxWidthWrapper>
 
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
-          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gpa-y-0">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
             {perks.map(perk => (
               <div
                 key={perk.name}
@@ -61,7 +69,7 @@ export default function Home() {
                   </div>
                 </div>
 
-                <div className="mt-6 md:ml04 md:mt-0 lg:ml-0 lg:mt-6">
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
                   <h3 className="text-base font-medium text-gray-900">
                     {perk.name}
                   </h3>
